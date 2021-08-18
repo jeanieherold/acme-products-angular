ab8d9a794138db8a4fbd082bf9277e7e0b4aa950
“jeanieherold” <jeaniemherold@gmail.com>
2021-08-18 16:08:51 -0400

    save'

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -9,8 +9,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct | undefined;
+  imageWidth: number = 200;
+  imageMargin: number = 2;
 
   //inject ActivatedRoute as a dependency in this component
+  //the router is a route defined in code like a back button using the onBack method
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
@@ -18,6 +21,18 @@ export class ProductDetailComponent implements OnInit {
     //since url wont change while user is here - use snapshot approach
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+
+    //hard coding a product for testing:
+    this.product = {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2021',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    };
   }
 
   onBack(): void {
